463bcb2cbaeb274dacce350011b43509d0467694
Ali Addokhi <aaddokhi@endoprognosis.org>
2026-05-08 15:23:14 +0300

    fix: complete firebase environment variables

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVfaDk6jrnkUt1MKgi3fbdslPiyj8f3Vo",
-  authDomain: "endoprognosis-prod.firebaseapp.com",
-  projectId: "endoprognosis-prod",
-  storageBucket: "endoprognosis-prod.firebasestorage.app",
-  messagingSenderId: "130485316465",
-  appId: "1:130485316465:web:67c6c2dba9f6a8caabe4d0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
